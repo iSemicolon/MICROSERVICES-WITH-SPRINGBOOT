5449c99d9659a705fa6ad55506276d9aeff4351f
iSemicolon <palash.samanta7497@gmail.com>
2023-01-12 18:41:35 +0530

    Employee-Service With ModelMapper

--- employee-service/src/main/java/org/kol/employeeApp/service/EmployeeService.java
+++ employee-service/src/main/java/org/kol/employeeApp/service/EmployeeService.java
@@ -3,6 +3,7 @@ package org.kol.employeeApp.service;
 import org.kol.employeeApp.entity.Employee;
 import org.kol.employeeApp.repository.EmployeeRepo;
 import org.kol.employeeApp.response.EmployeeResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,17 +16,25 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepo employeeRepo;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public EmployeeResponse getEmployeesById(int id) {
 
         //set data by rest api call
         Employee employee=employeeRepo.findById(id).get(); //dbcall
 
-        EmployeeResponse employeeResponse= new EmployeeResponse();
+//        EmployeeResponse employeeResponse= new EmployeeResponse();
+//        employeeResponse.setId(employee.getId());
+//        employeeResponse.setName(employee.getName());
+//        employeeResponse.setEmail(employee.getEmail());
+//        employeeResponse.setBloodGroup(employee.getBloodGroup());
 
-        employeeResponse.setId(employee.getId());
-        employeeResponse.setName(employee.getName());
-        employeeResponse.setEmail(employee.getEmail());
-        employeeResponse.setBloodGroup(employee.getBloodGroup());
+        //Replaced commented code with ModelMapper
+
+        EmployeeResponse employeeResponse=  modelMapper.map(employee, EmployeeResponse.class);
+
+
 
 
        return employeeResponse;
