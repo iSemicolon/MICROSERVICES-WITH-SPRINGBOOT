9eac3817400e4f6d4ed336233b4cf34d2040b216
iSemicolon <palash.samanta7497@gmail.com>
2023-01-12 18:13:59 +0530

    Address-Service Ready

---
+++ address-service/src/main/java/org/kol/addressApp/service/AddressService.java
@@ -0,0 +1,33 @@
+package org.kol.addressApp.service;
+
+import org.kol.addressApp.entity.Address;
+import org.kol.addressApp.repository.AddressRepo;
+import org.kol.addressApp.response.AddressResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    private AddressRepo addressRepo;
+
+    public AddressResponse findAddresById(int id) {
+
+        //set data by rest api call
+        Address address=addressRepo.findById(id).get(); //dbcall
+
+        AddressResponse addressResponse=new AddressResponse();
+        addressResponse.setId(address.getId());
+        addressResponse.setLane1(address.getLane1());
+        addressResponse.setLane2(address.getLane2());
+        addressResponse.setZip(address.getZip());
+        addressResponse.setState(address.getState());
+
+
+       return addressResponse;
+
+   }
+
+
+   }
