8c39e511dc435f1ca700483a9742600e2c114a09
iSemicolon <palash.samanta7497@gmail.com>
2023-01-12 18:50:59 +0530

    Address-Service With ModelMapper

--- address-service/src/main/java/org/kol/addressApp/service/AddressService.java
+++ address-service/src/main/java/org/kol/addressApp/service/AddressService.java
@@ -3,6 +3,7 @@ package org.kol.addressApp.service;
 import org.kol.addressApp.entity.Address;
 import org.kol.addressApp.repository.AddressRepo;
 import org.kol.addressApp.response.AddressResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +13,24 @@ public class AddressService {
     @Autowired
     private AddressRepo addressRepo;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public AddressResponse findAddresById(int id) {
 
         //set data by rest api call
-        Address address=addressRepo.findById(id).get(); //dbcall
+       Address address=addressRepo.findById(id).get(); //dbcall
+//
+//        AddressResponse addressResponse=new AddressResponse();
+//        addressResponse.setId(address.getId());
+//        addressResponse.setLane1(address.getLane1());
+//        addressResponse.setLane2(address.getLane2());
+//        addressResponse.setZip(address.getZip());
+//        addressResponse.setState(address.getState());
 
-        AddressResponse addressResponse=new AddressResponse();
-        addressResponse.setId(address.getId());
-        addressResponse.setLane1(address.getLane1());
-        addressResponse.setLane2(address.getLane2());
-        addressResponse.setZip(address.getZip());
-        addressResponse.setState(address.getState());
+        //Replaced commented lines with ModelMapper
 
+        AddressResponse addressResponse=  modelMapper.map(address, AddressResponse.class);
 
        return addressResponse;
 
